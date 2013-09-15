55890a6474d5f2616383c8705b181a3d44eaa74c
intoxicated <yakle1218@gmail.com>
2013-09-14 20:49:36 -0400

    Add properties that can be used throughout project

--- Iircutils/Iircutils.h
+++ Iircutils/Iircutils.h
@@ -5,9 +5,11 @@
 //  Created by Realfree on 9/14/13.
 //  Copyright (c) 2013 Realfree. All rights reserved.
 //
+//  Lightweight singleton to provide useful data structures
 
 #import <Foundation/Foundation.h>
 
 @interface Iircutils : NSObject
-
+@property (strong, nonatomic) NSMutableDictionary * name_symbols;
+@property (strong, nonatomic) NSArray * commands_no_target;
 @end
